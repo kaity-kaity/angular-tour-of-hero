4e9a8b8346a272e76a7edac59efc086109cd85bc
kaito0702 <kaito.kitagawa.biz@gmail.com>
2022-06-12 14:11:06 +0900

    コードの完全移行

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Ogawa' },
+    { id: 12, name: 'Ozaki' },
+    { id: 13, name: 'Azumi' },
+    { id: 14, name: 'Take' },
+    { id: 15, name: 'Horiuchi' },
+    { id: 16, name: 'Asai' },
+    { id: 17, name: 'Tomioka' },
+    { id: 18, name: 'Huruya' },
+    { id: 19, name: 'Kimura' },
+    { id: 20, name: 'Kono' },
+    { id: 21, name: 'Takio' },
+    { id: 22, name: 'Kitagawa' },
+]
